349091b64d265443d45ace8265d79eb5ad597235
Md Nayeem Hossain <nayeemhossain108@gmail.com>
2021-11-30 23:46:06 +0600

    added

--- src/components/Testimonial/Testimonial.js
+++ src/components/Testimonial/Testimonial.js
@@ -7,7 +7,7 @@ const Testimonial = () => {
              <h2 className="fw-bold mb-5  ">Testimonial <br />
             <i class="far fa-window-minimize"></i>
             </h2>
-            <Carosel></Carosel>
+            
 
         </div>
     );
